6a04fcf4fa1b4a1dd646e65abe0926cc1287fccd
Gustavo Silva <gustagui.dev@gmail.com>
2021-09-23 22:16:53 -0300

    config sequelize

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,9 +1,29 @@
+const { User } = require('../models')
+
 const authController = {
   showLogin(req, res) {
     return res.render("auth/login");
   },
   showRegister(req, res) {
     return res.render("auth/register");
+  },
+  async register(req, res) {
+    const { name, email, password, username } = req.body;
+    try {
+      const user = await User.create({
+        name,
+        email,
+        password,
+        username,
+        avatar: "link",
+        create_at: new Date().toISOString(),
+      });
+
+      return res.redirect("/login");
+    } catch (err) {
+      console.log(err);
+      return res.redirect("/registro");
+    }
   },
 };
 
